fcb69a1121010af05d203c77ff6f6f46cd775bb0
szoverfi <136331532+Easybisi@users.noreply.github.com>
2024-12-11 14:53:26 +0100

    4

--- task4/src/App.jsx
+++ task4/src/App.jsx
@@ -1,4 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React,{useEffect,useState} from "react";
+
+
 export default function App() {
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    fetch("/api/books")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`hmmmmmm: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("nem jo:", error));
+  }, []);
+  console.log(books);
+
+ 
+  
   return <h1>It Works! ⚙️</h1>
 }
 
